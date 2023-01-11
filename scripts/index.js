08662e8031aab20c07dc1d9aee087f7d38cb041f
iamgauhar <iamgauhar@yahoo.com>
2023-01-12 01:04:52 +0530

    Added skiils

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,25 @@
+let headerr = document.getElementById("header");
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset >= 250) {
+    headerr.classList.add("stick");
+  } else {
+    headerr.classList.remove("stick");
+  }
+});
+
+let menubar = document.querySelector(".menuList");
+let close = document.querySelector(".menuList");
+const openMenu = () => {
+  menubar.style.left = "0";
+};
+const closeMenu = () => {
+  menubar.style.left = "-100%";
+};
+
+const goTo = () => {
+  window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+};
+
 document.querySelector("#resume").addEventListener("click", () => {
   // location.href = "https://drive.google.com/file/d/1xr0EwCPD00gk6ilTQ8BAvheR3HTUYnDp/view?usp=share_link";
   window.open(
@@ -6,6 +28,7 @@ document.querySelector("#resume").addEventListener("click", () => {
   );
 });
 
+AOS.init();
 // Navbar script
 // document.querySelector("canvas").style.position = "relative";
 // const nav = function () {
