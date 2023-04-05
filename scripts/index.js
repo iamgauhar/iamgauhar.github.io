8399855ca687a08b257eab522780194451dd84f1
iamgauhar <iamgauhar@yahoo.com>
2023-04-05 14:50:24 +0530

    fixing rsume 2

--- scripts/index.js
+++ scripts/index.js
@@ -22,9 +22,9 @@ const goTo = () => {
 
 document.querySelector("#resume").addEventListener("click", () => {
   // location.href = "https://drive.google.com/file/d/1xr0EwCPD00gk6ilTQ8BAvheR3HTUYnDp/view?usp=share_link";
-  window.open(
-    "https://drive.google.com/u/0/uc?id=1xr0EwCPD00gk6ilTQ8BAvheR3HTUYnDp&export=download",
-  );
+  // window.open(
+  //   "https://drive.google.com/u/0/uc?id=1xr0EwCPD00gk6ilTQ8BAvheR3HTUYnDp&export=download",
+  // );
   // window.open(
   //   "https://drive.google.com/file/d/1xr0EwCPD00gk6ilTQ8BAvheR3HTUYnDp/view?usp=share_link",
   //   "_blank"
